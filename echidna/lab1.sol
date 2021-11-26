e79e7db7f0afa1f0a1b13c969a0aa213b5713945
y0wl <yowl@arbitraryexecution.com>
2021-11-26 11:24:47 -0500

    burn method

--- echidna/lab1.sol
+++ echidna/lab1.sol
@@ -14,6 +14,7 @@ contract SimpleBank {
      */
     function deposit() public payable {
         balances[msg.sender] += msg.value;
+        // might have to add something into here
     }
 
     /**
@@ -36,12 +37,15 @@ contract SimpleBank {
      */
     function burn(uint256 amount) public {
         balances[msg.sender] -= amount;
+
+        // check for underflow
+        require(int256(balances[msg.sender]) >= 0, "Underflow detected! Reverting!");
     }
 
-    /**
-     * @dev Return value in the bank
-     */
-    function TVL() public view returns(uint256){
-        return address(this).balance;
+    // *** ECHIDNA FUZZING BELOW ***
+    uint256 totalDeposited = 0;
+
+    function echidna_test() public returns (bool){
+        // implement echidna logic here
     }
 }
